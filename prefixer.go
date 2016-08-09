b187c2e86cf01eb6b879be24862c5d9489dcd79d
Karrick S. McDermott <kmcdermott@linkedin.com>
2016-08-09 00:05:36 +0000

    uses container.Ring for roundRobin

--- prefixer.go
+++ prefixer.go
@@ -4,8 +4,11 @@ import "net/http"
 
 // Prefixer prepends the specified prefix to the URL before redirecting to the underlying Getter.
 type Prefixer struct {
-	Prefix string
+	// The object whose Get method is invoked to get results for a query.
 	Getter Getter
+
+	// Prefix for each query, commonly the hostname.
+	Prefix string
 }
 
 // Get prepends the specified prefix to the URL before redirecting to the underlying Getter.
--- roundRobin.go
+++ roundRobin.go
@@ -1,25 +1,28 @@
 package gogetter
 
 import (
+	"container/ring"
 	"net/http"
-	"sync"
 )
 
 // RoundRobin responds to the Get method by invoking all specified Getters in round-robin fashion.
-type RoundRobin struct {
-	Getters   []Getter
-	indexLock sync.Mutex
-	index     uint64
+type roundRobin struct {
+	r *ring.Ring
+}
+
+// NewRoundRobin returns a Getter that sends successive queries to all the Getters its list.
+func NewRoundRobin(getters []Getter) Getter {
+	r := ring.New(len(getters))
+
+	// log.Printf("servers: %v\n", getters)
+	for i := 0; i < len(getters); i++ {
+		r.Next().Value = getters[i]
+	}
+
+	return &roundRobin{r: r}
 }
 
 // Get responds to the Get method by invoking all specified Getters in round-robin fashion.
-func (g *RoundRobin) Get(url string) (*http.Response, error) {
-	g.indexLock.Lock()
-	index := g.index
-	g.index++
-	if g.index == uint64(len(g.Getters)) {
-		g.index = 0
-	}
-	g.indexLock.Unlock() // NOTE: indexLock is not unlocked using defer because the Get invocation below can take quite a bit of time.
-	return g.Getters[index].Get(url)
+func (g *roundRobin) Get(url string) (*http.Response, error) {
+	return g.r.Next().Value.(Getter).Get(url)
 }
